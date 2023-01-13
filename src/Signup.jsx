b585c58748a2b262d45d1c9fb3832562ca211cdc
JRisJunior <jeffmrubin@gmail.com>
2023-01-13 12:34:35 -0600

    change signup  html form to use javascript

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,6 +1,22 @@
+import axios from 'axios';
+
 export function Signup() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+    const params = new FormData(event.target);
+    axios.post("http://localhost:3000/users", params).then(response => {
+      console.log(response.data);
+      event.target.reset();
+    })
+      .catch(error => {
+        console.log("in the catch");
+        console.log(error.response.data.errors);
+      });
+  };
+
   return (
-    <form action="http://localhost:3000/users" method="POST">
+    <form onSubmit={handleSubmit}>
       <p>Name: <input type="text" name="name"/></p>
       <p>Email: <input type="email" name="email"/></p>
       <p>Password: <input type="password" name="password"/></p>
